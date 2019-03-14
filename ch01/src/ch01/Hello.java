b043b13ad0463263c2aecf8c8579fd5f54cc69b0
witch49 <verdure789@gmail.com>
2019-03-14 10:39:26 +0900

    first commit

--- ch01/src/ch01/Hello.java
+++ ch01/src/ch01/Hello.java
@@ -2,6 +2,11 @@ package ch01;
 
 public class Hello {
 	public static void main(String[] args) {
+		/*
+		 * How to fix the garbled letters Window > Preferences > General > workspace >
+		 * Text file encoding : other - UTF-8
+		 */
+		
 		System.out.println("Hello, Java World!");
 		System.out.println("하이요 자바 월드");
 	}
