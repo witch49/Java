dd2bc8f5fdc80dc417f61013455a22d8967635a0
witch49 <verdure789@gmail.com>
2019-03-15 10:55:21 +0900

    Chapter03

---
+++ ch03/src/exercise/Exercise04.java
@@ -0,0 +1,9 @@
+package exercise;
+
+public class Exercise04 {
+	public static void main(String[] args) {
+		int value = 356;
+		System.out.println(value-(value%100));
+
+	}
+}
---
+++ ch03/src/exercise/Exercise05.java
@@ -0,0 +1,12 @@
+package exercise;
+
+public class Exercise05 {
+	public static void main(String[] args) {
+		// 사다리꼴
+		int lengthTop = 5;
+		int lengthBottom = 10;
+		int height = 7;
+		double area = (lengthTop + lengthBottom) / 2 * height;
+		System.out.println(area);
+	}
+}
