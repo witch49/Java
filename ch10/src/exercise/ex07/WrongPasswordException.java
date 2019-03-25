a8dc4c31568c6a410eb41c6ff717c454918c1489
witch49 <verdure789@gmail.com>
2019-03-25 12:06:25 +0900

    Chapter10

---
+++ ch10/src/exercise/ex07/WrongPasswordException.java
@@ -0,0 +1,11 @@
+package exercise.ex07;
+
+public class WrongPasswordException extends Exception {
+	public WrongPasswordException() {
+
+	}
+
+	public WrongPasswordException(String message) {
+		super(message);
+	}
+}
---
+++ ch10/src/sec02/exam04/ClassCastExceptionExample.java
@@ -0,0 +1,27 @@
+package sec02.exam04;
+
+public class ClassCastExceptionExample {
+	public static void main(String[] args) {
+		Dog dog = new Dog();
+		changeDog(dog);
+
+		Cat cat = new Cat();
+		changeDog(cat);
+	}
+
+	public static void changeDog(Animal animal) {
+		if (animal instanceof Dog) {
+			Dog dog = (Dog) animal; // ClassCastException 발생
+		}
+	}
+
+}
+
+class Animal {
+}
+
+class Dog extends Animal {
+}
+
+class Cat extends Animal {
+}
